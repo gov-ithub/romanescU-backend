3d41e493919f3deaebfb52bf305d86b24ec57ecd
Mihnea <lzr_mihnea@yahoo.com>
2016-11-05 13:36:09 +0200

    Service for Ministry of External Affairs added

---
+++ src/main/java/romanescu/backend/service/MinOfExtAfService.java
@@ -0,0 +1,7 @@
+package romanescu.backend.service;
+
+/**
+ * Created by Mihnea on 11/5/16.
+ */
+public interface MinOfExtAfService {
+}
---
+++ src/main/java/romanescu/backend/service/impl/MinOfExtAfServiceImpl.java
@@ -0,0 +1,7 @@
+package romanescu.backend.service.impl;
+
+/**
+ * Created by Mihnea on 11/5/16.
+ */
+public class MinOfExtAfServiceImpl {
+}
